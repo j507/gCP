9341c080ebd534520ce254dac9e86fbf6a93bf48
j507 <jose.a.lara.h@gmail.com>
2022-05-09 21:04:08 +0200

    First draft of the HookLaw<dim> class

--- include/gCP/constitutive_equations.h
+++ include/gCP/constitutive_equations.h
@@ -1,15 +1,98 @@
 #ifndef INCLUDE_CONSTITUTIVE_EQUATIONS_H_
 #define INCLUDE_CONSTITUTIVE_EQUATIONS_H_
 
-
+#include <gCP/crystal_data.h>
 
 #include <deal.II/base/symmetric_tensor.h>
+
+#include <deal.II/fe/fe_values.h>
+
+#include <deal.II/lac/generic_linear_algebra.h>
 
 
 
 namespace gCP
 {
 
+
+
+namespace Kinematics
+{
+
+
+template <int dim>
+class ElasticStrainTensor
+{
+public:
+  ElasticStrainTensor();
+
+  const std::vector<dealii::SymmetricTensor<2,dim>> get_elastic_strain_tensor(
+    const dealii::LinearAlgebraTrilinos::MPI::Vector  solution,
+    const dealii::FEValues<dim>                       &fe_values,
+    const dealii::types::material_id                  crystal_id) const;
+
+private:
+
+  std::shared_ptr<CrystalsData<dim>>              crystals_data;
+
+  std::shared_ptr<std::vector<int>>               displacements_extractors;
+
+  std::shared_ptr<std::vector<std::vector<int>>>  slips_extractors;
+};
+
+template <int dim>
+const std::vector<dealii::SymmetricTensor<2,dim>>
+ElasticStrainTensor<dim>::get_elastic_strain_tensor(
+  const dealii::LinearAlgebraTrilinos::MPI::Vector  solution,
+  const dealii::FEValues<dim>                       &fe_values,
+  const dealii::types::material_id                  crystal_id) const
+{
+  const unsigned int n_q_points = fe_values.n_quadrature_points;
+
+  std::vector<dealii::SymmetricTensor<2,dim>> strain_tensor_values(
+    n_q_points,
+    dealii::SymmetricTensor<2,dim>());
+
+  std::vector<dealii::SymmetricTensor<2,dim>> elastic_strain_tensor_values(
+    n_q_points,
+    dealii::SymmetricTensor<2,dim>());
+
+  std::vector<dealii::SymmetricTensor<2,dim>> plastic_strain_tensor_values(
+    n_q_points,
+    dealii::SymmetricTensor<2,dim>());
+
+  std::vector<double>                         slip_values(n_q_points, 0);
+
+  dealii::SymmetricTensor<2,dim>              schmid_tensor();
+
+  fe_values[(*displacements_extractors)[crystal_id]]->get_function_symmetric_gradients(
+    solution,
+    strain_tensor_values);
+
+  elastic_strain_tensor_values  = strain_tensor_values;
+
+  for (unsigned int slip_id = 0; slip_id < (*crystals_data)[crystal_id].n_slips; ++slip_id)
+  {
+    fe_values[(*slips_extractors)[crystal_id][slip_id]]->get_function_value(
+      solution,
+      slip_values);
+
+    schmid_tensor =
+      crystals_data.get_symmetrized_schmid_tensor(crystal_id, slip_id);
+
+    for (unsigned int q_point = 0; q_point < n_q_points; ++q_point)
+    {
+      elastic_strain_tensor_values[q_point] -=
+        slip_values[q_point] * schmid_tensor;
+    }
+  }
+
+  return elastic_strain_tensor_values;
+}
+
+
+
+} // namespace Kinematics
 
 
 namespace ConstitutiveEquations
@@ -24,23 +107,49 @@ public:
   HookeLaw(const double youngs_modulus,
            const double poissons_ratio);
 
-  void compute_stiffness_tetrad();
+  HookeLaw(const std::shared_ptr<CrystalsData<dim>> &crystals_data,
+           const double                             C_1111,
+           const double                             C_1212,
+           const double                             C_1122);
+
+  void init();
+
+  void compute_reference_stiffness_tetrad();
 
   const dealii::SymmetricTensor<4,dim> &get_stiffness_tetrad() const;
+
+  const dealii::SymmetricTensor<4,dim> &get_stiffness_tetrad(
+    const unsigned int crystal_id) const;
 
   const dealii::SymmetricTensor<2,dim> compute_stress_tensor(
     const dealii::SymmetricTensor<2,dim> strain_tensor_values) const;
 
+  const dealii::SymmetricTensor<2,dim> compute_stress_tensor(
+    const unsigned int                    crystal_id,
+    const dealii::SymmetricTensor<2,dim>  strain_tensor_values) const;
+
 private:
-  const double                          bulk_modulus;
+  enum class CrystalSystem
+  {
+    Isotropic,
+    Cubic
+  };
 
-  const double                          shear_modulus;
+  std::shared_ptr<CrystalsData<dim>>          crystals_data;
 
-  const dealii::SymmetricTensor<4,dim>  spherical_projector;
+  CrystalSystem                               crystal_system;
 
-  const dealii::SymmetricTensor<4,dim>  deviatoric_projector;
+  const double                                C_1111;
 
-  dealii::SymmetricTensor<4,dim>        stiffness_tetrad;
+  const double                                C_1212;
+
+  const double                                C_1122;
+
+  dealii::SymmetricTensor<4,dim>              reference_stiffness_tetrad;
+
+  std::vector<dealii::SymmetricTensor<4,dim>> stiffness_tetrads;
+
+  bool                                        flag_init_was_called;
 };
 
 
@@ -49,9 +158,17 @@ template <int dim>
 inline const dealii::SymmetricTensor<4,dim>
 &HookeLaw<dim>::get_stiffness_tetrad() const
 {
-  return (stiffness_tetrad);
+  return (reference_stiffness_tetrad);
 }
 
+
+
+template <int dim>
+inline const dealii::SymmetricTensor<4,dim>
+&HookeLaw<dim>::get_stiffness_tetrad(const unsigned int crystal_id) const
+{
+  return (stiffness_tetrads[crystal_id]);
+}
 
 
 template<int dim, int n_slips>
--- src/constitutive_equations.cc
+++ src/constitutive_equations.cc
@@ -1,6 +1,6 @@
 #include <gCP/constitutive_equations.h>
 
-
+#include <deal.II/base/symmetric_tensor.h>
 
 namespace gCP
 {
@@ -16,32 +16,126 @@ template<int dim>
 HookeLaw<dim>::HookeLaw(const double youngs_modulus,
                         const double poissons_ratio)
 :
-bulk_modulus(youngs_modulus/(3.0 * (1.0 - 2.0 * poissons_ratio))),
-shear_modulus(youngs_modulus/(2.0 * (1.0 + poissons_ratio))),
-spherical_projector(1.0 / 3.0 * dealii::outer_product(
-                                  dealii::unit_symmetric_tensor<dim>(),
-                                  dealii::unit_symmetric_tensor<dim>())),
-deviatoric_projector(dealii::identity_tensor<dim>() - spherical_projector)
-{
-  compute_stiffness_tetrad();
-}
+crystal_system(CrystalSystem::Isotropic),
+C_1111(youngs_modulus * poissons_ratio /
+       ((1.0 + poissons_ratio) * (1.0 - 2.0 * poissons_ratio))
+       +
+       youngs_modulus / (1.0 + poissons_ratio)),
+C_1212(youngs_modulus / 2.0 / (1.0 + poissons_ratio)),
+C_1122(youngs_modulus * poissons_ratio /
+       ((1.0 + poissons_ratio) * (1.0 - 2.0 * poissons_ratio))),
+flag_init_was_called(false)
+{}
 
 
 
 template<int dim>
-void HookeLaw<dim>::compute_stiffness_tetrad()
+HookeLaw<dim>::HookeLaw(
+  const std::shared_ptr<CrystalsData<dim>> &crystals_data,
+  const double                             C_1111,
+  const double                             C_1212,
+  const double                             C_1122)
+:
+crystals_data(crystals_data),
+crystal_system(CrystalSystem::Cubic),
+C_1111(C_1111),
+C_1212(C_1212),
+C_1122(C_1122),
+flag_init_was_called(false)
+{}
+
+
+
+template<int dim>
+void HookeLaw<dim>::init()
 {
-  stiffness_tetrad = 3.0 * bulk_modulus * spherical_projector
-                      + 2.0 * shear_modulus * deviatoric_projector;
+  for (unsigned int i = 0; i < dim; i++)
+    for (unsigned int j = 0; j < dim; j++)
+      for (unsigned int k = 0; k < dim; k++)
+        for (unsigned int l = 0; l < dim; l++)
+          if (i == j && j == k && k == l)
+            reference_stiffness_tetrad[i][j][k][l] = C_1111;
+          else if (i == k && j == l)
+            reference_stiffness_tetrad[i][j][k][l] = C_1212;
+          else if (i == j && k == l)
+            reference_stiffness_tetrad[i][j][k][l] = C_1122;
+
+  switch (crystal_system)
+  {
+  case CrystalSystem::Isotropic:
+    break;
+
+  case CrystalSystem::Cubic:
+    {
+      AssertThrow(crystals_data->is_initialized(),
+                  dealii::ExcMessage("The underlying CrystalsData<dim>"
+                                     " instance has not been "
+                                     " initialized."));
+
+      for (unsigned int crystal_id = 0;
+           crystal_id < crystals_data->get_n_slips();
+           crystal_id++)
+        {
+          dealii::SymmetricTensor<4,dim> stiffness_tetrad;
+
+          dealii::Tensor<2,dim> rotation_tensor =
+            crystals_data->get_rotation_tensor(crystal_id);
+
+          for (unsigned int i = 0; i < dim; i++)
+            for (unsigned int j = 0; j < dim; j++)
+              for (unsigned int k = 0; k < dim; k++)
+                for (unsigned int l = 0; l < dim; l++)
+                  for (unsigned int o = 0; o < dim; o++)
+                    for (unsigned int p = 0; p < dim; p++)
+                      for (unsigned int q = 0; q < dim; q++)
+                        for (unsigned int r = 0; r < dim; r++)
+                          stiffness_tetrad[i][j][k][l] =
+                            rotation_tensor[i][o] *
+                            rotation_tensor[j][p] *
+                            rotation_tensor[k][q] *
+                            rotation_tensor[l][r] *
+                            reference_stiffness_tetrad[o][p][q][r];
+
+          stiffness_tetrads.push_back(stiffness_tetrad);
+        }
+    }
+    break;
+
+  default:
+    break;
+  }
+
+  flag_init_was_called = true;
+}
+
+
+template<int dim>
+const dealii::SymmetricTensor<2,dim> HookeLaw<dim>::
+compute_stress_tensor(
+  const dealii::SymmetricTensor<2,dim> strain_tensor_values) const
+{
+  return reference_stiffness_tetrad * strain_tensor_values;
 }
 
 
 
 template<int dim>
 const dealii::SymmetricTensor<2,dim> HookeLaw<dim>::
-compute_stress_tensor(const dealii::SymmetricTensor<2,dim> strain_tensor_values) const
+compute_stress_tensor(
+  const unsigned int                    crystal_id,
+  const dealii::SymmetricTensor<2,dim>  strain_tensor_values) const
 {
-  return stiffness_tetrad * strain_tensor_values;
+  AssertThrow(crystals_data.get() != nullptr,
+              dealii::ExcMessage("This overloaded method requires a "
+                                 "constructor called where a "
+                                 "CrystalsData<dim> instance is "
+                                 "passed as a std::shared_ptr"))
+
+  dealii::ExcIndexRangeType<int>(crystal_id,
+                                 0,
+                                 crystals_data->get_n_crystals());
+
+  return stiffness_tetrads[crystal_id] * strain_tensor_values;
 }
 
 
