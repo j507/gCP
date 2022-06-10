836e5da33768c2c9bdcdcd4132b604f0016535fd
j507 <jose.a.lara.h@gmail.com>
2022-06-10 18:01:28 +0200

    Fixed compilation error due to missing source file

---
+++ src/utilities.cc
@@ -0,0 +1,91 @@
+
+#include <gCP/utilities.h>
+
+
+
+namespace gCP
+{
+
+
+
+namespace Utilities
+{
+
+
+
+Logger::Logger(const std::string output_filepath)
+:
+output_filepath(output_filepath)
+{}
+
+
+
+void Logger::declare_column(const std::string column_name)
+{
+  for (const auto& pair : data_map)
+      AssertThrow(pair.first != column_name,
+                  dealii::ExcMessage("This column was already defined!"))
+
+  data_map[column_name] = std::make_pair(0.0, false);
+}
+
+
+
+void Logger::update_value(const std::string column_name,
+                          const double value)
+{
+  AssertIsFinite(value);
+
+  data_map[column_name].first = value;
+}
+
+
+
+void Logger::set_scientific(const std::string column_name,
+                            const bool boolean)
+{
+  data_map[column_name].second = boolean;
+}
+
+
+
+void Logger::log_to_file()
+{
+  output_filepath << std::string(2, ' ');
+
+  for (const auto& pair : data_map)
+  {
+    if (pair.second.second)
+      output_filepath << std::fixed << std::scientific;
+    else
+      output_filepath << std::defaultfloat;
+
+    output_filepath << std::setw(pair.first.length()) << std::right
+                    << std::setprecision(6)
+                    << pair.second.first << std::string(5, ' ');
+  }
+
+  output_filepath << std::endl;
+}
+
+
+
+void Logger::add_break(const std::string message)
+{
+  output_filepath << std::endl
+                  << message
+                  << std::endl << std::endl
+                  << std::string(2, ' ');
+
+  for (const auto& pair : data_map)
+    output_filepath << pair.first << std::string(5, ' ');
+
+  output_filepath << std::endl;
+}
+
+} // namespace Utilities
+
+
+
+} // namespace gCP
+
