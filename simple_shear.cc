b7b08ea3a3bc8c9c8f517f01b8cebef62b6fdd32
j507 <jose.a.lara.h@gmail.com>
2022-08-01 10:55:15 +0200

    Added line specifiyng which input file is being used

--- simple_shear.cc
+++ simple_shear.cc
@@ -909,6 +909,14 @@ int main(int argc, char *argv[])
       break;
     }
 
+    int rank;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (rank == 0)
+      std::cout << "Running with \""
+                << parameters_filepath << "\"" << "\n";
+
     gCP::RunTimeParameters::SimpleShearParameters parameters(parameters_filepath);
 
     gCP::SimpleShearProblem<2> problem(parameters);
