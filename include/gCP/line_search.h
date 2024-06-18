1c576a911ae3134705b58856339c18b636b414aa
j507 <jose.a.lara.h@gmail.com>
2024-06-18 12:56:03 +0200

    (A) Methods and members for indpendent line search (Not yet fully tested)

--- include/gCP/line_search.h
+++ include/gCP/line_search.h
@@ -8,112 +8,213 @@
 #include <deal.II/lac/full_matrix.h>
 #include <deal.II/lac/vector.h>
 
-
-
 namespace gCP
 {
 
-
-/*!
- * @brief Line search algorithmus
- * @details The algorithmus mirrors that shown in chapter 6.5
- * "Global methods for systems of nonlinear equations" of
- * https://doi.org/10.1137/1.9781611971200
- *
- * @todo Docu
- */
-class LineSearch
-{
-public:
-  LineSearch();
-
-  LineSearch(const RunTimeParameters::LineSearchParameters &parameters);
-
-  void reinit(const double initial_scalar_function_value,
-              const unsigned int step_id = 0,
-              const unsigned int iteration_id = 0);
-
-  bool suficient_descent_condition(
-    const double trial_scalar_function_value,
-    const double lambda) const;
-
-  double get_lambda(const double trial_scalar_function_value,
-                    const double current_lambda);
-
-  unsigned int get_n_iterations() const;
-
-  void write_to_file(const std::string filepath);
-
-private:
-  unsigned int          n_iterations;
-
-  const unsigned int    n_max_iterations;
-
-  const double          alpha;
-
-  const double          beta;
-
-  double                initial_scalar_function_value;
-
-  double                descent_direction;
-
-  double                lambda;
-
-  double                old_lambda;
-
-  double                old_old_lambda;
-
-  double                old_scalar_function_value;
-
-  double                old_old_scalar_function_value;
-
-  unsigned int          step_id;
-
-  unsigned int          iteration_id;
-
-  dealii::TableHandler  table_handler;
-
-  double quadratic_backtracking(
-    const double trial_scalar_function_value);
-
-  double cubic_backtracking();
-};
-
-
-
-inline bool
-LineSearch::suficient_descent_condition(
-  const double trial_scalar_function_value,
-  const double lambda) const
-{
-  if (n_iterations > n_max_iterations)
+  /*!
+   * @brief Line search algorithmus
+   * @details The algorithmus mirrors that shown in chapter 6.5
+   * "Global methods for systems of nonlinear equations" of
+   * https://doi.org/10.1137/1.9781611971200
+   *
+   * @todo Docu
+   */
+  class LineSearch
   {
-    std::cout
-      << "Warning: The maximal number of iterations of the line search"
-      << " algorithm has been reached" << std::endl;
+  public:
+    LineSearch();
 
-    return true;
-  }
-  else
+    LineSearch(const RunTimeParameters::LineSearchParameters &parameters);
+
+    void reinit(const double initial_scalar_function_value,
+                const unsigned int step_id = 0,
+                const unsigned int iteration_id = 0);
+
+    void reinit(const std::vector<double> initial_scalar_function_values,
+                const unsigned int step_id = 0,
+                const unsigned int iteration_id = 0);
+
+    bool suficient_descent_condition(
+        const double trial_scalar_function_value,
+        const double lambda) const;
+
+    bool suficient_descent_condition(
+        const std::vector<double> trial_scalar_function_values,
+        const std::vector<double> lambdas);
+
+    double get_lambda(const double trial_scalar_function_value,
+                      const double current_lambda);
+
+    std::vector<double> get_lambdas(
+        const std::vector<double> trial_scalar_function_values,
+        const std::vector<double> current_lambdas);
+
+    unsigned int get_n_iterations() const;
+
+    static double get_objective_function_value(const double residuum);
+
+    static std::vector<double> get_objective_function_values(
+        const std::vector<double> residuum);
+
+    void write_to_file(const std::string filepath);
+
+  private:
+    unsigned int n_iterations;
+
+    const unsigned int n_max_iterations;
+
+    const double alpha;
+
+    const double beta;
+
+    double initial_scalar_function_value;
+
+    std::vector<double> initial_scalar_function_values;
+
+    double descent_direction;
+
+    std::vector<double> descent_directions;
+
+    double lambda;
+
+    std::vector<double> lambdas;
+
+    double old_lambda;
+
+    std::vector<double> old_lambdas;
+
+    double old_old_lambda;
+
+    std::vector<double> old_old_lambdas;
+
+    double old_scalar_function_value;
+
+    std::vector<double> old_scalar_function_values;
+
+    double old_old_scalar_function_value;
+
+    std::vector<double> old_old_scalar_function_values;
+
+    std::vector<bool> flag_sufficient_descent_conditions;
+
+    unsigned int step_id;
+
+    unsigned int iteration_id;
+
+    dealii::TableHandler table_handler;
+
+    double quadratic_backtracking(
+        const double trial_scalar_function_value);
+
+    double quadratic_backtracking(
+        const double trial_scalar_function_value,
+        const unsigned int id);
+
+    double cubic_backtracking();
+
+    double cubic_backtracking(const unsigned int id);
+  };
+
+  inline bool
+  LineSearch::suficient_descent_condition(
+      const double trial_scalar_function_value,
+      const double lambda) const
   {
-    return (trial_scalar_function_value <
+    if (n_iterations > n_max_iterations)
+    {
+      std::cout
+          << "Warning: The maximal number of iterations of the line search"
+          << " algorithm has been reached" << std::endl;
+
+      return true;
+    }
+    else
+    {
+      /*const bool beta_condition =
+          trial_scalar_function_value > initial_scalar_function_value +
+                                            beta * lambda * descent_direction;
+
+      if (!beta_condition)
+      {
+        std::cout << "Second conditions not fulfilled" << std::endl;
+      }*/
+
+      return (trial_scalar_function_value <
               initial_scalar_function_value +
-                alpha*lambda*descent_direction);
+                  alpha * lambda * descent_direction);
+    }
   }
-}
+
+  inline bool
+  LineSearch::suficient_descent_condition(
+      const std::vector<double> trial_scalar_function_values,
+      const std::vector<double> lambdas)
+  {
+    if (n_iterations > n_max_iterations)
+    {
+      std::cout
+          << "Warning: The maximal number of iterations of the line search"
+          << " algorithm has been reached" << std::endl;
+
+      return true;
+    }
+    else
+    {
+      /*const bool beta_condition =
+        trial_scalar_function_value > initial_scalar_function_value +
+          beta*lambda*descent_direction;
+
+      if (!beta_condition)
+      {
+        std::cout << "Second conditions not fulfilled" << std::endl;
+      }*/
+
+      for (unsigned int i = 0;
+           i < flag_sufficient_descent_conditions.size();
+           ++i)
+      {
+        flag_sufficient_descent_conditions[i] =
+            trial_scalar_function_values[i] <
+            initial_scalar_function_values[i] +
+                alpha * lambdas[i] * descent_directions[i];
 
 
+        if (trial_scalar_function_values[i] == 0.0)
+        {
+          flag_sufficient_descent_conditions[i] = true;
+        }
+      }
 
-inline unsigned int
-LineSearch::get_n_iterations() const
-{
-  return (n_iterations);
-}
+      return (flag_sufficient_descent_conditions[0] &&
+              flag_sufficient_descent_conditions[1]);
+    }
+  }
 
+  inline double
+  LineSearch::get_objective_function_value(const double residuum)
+  {
+    return (0.5 * residuum * residuum);
+  }
 
+  inline std::vector<double>
+  LineSearch::get_objective_function_values(const std::vector<double> residuum)
+  {
+    std::vector<double> objective_functions(residuum.size());
+
+    for (unsigned int i = 0; i < residuum.size(); i++)
+    {
+      objective_functions[i] = 0.5 * residuum[i] * residuum[i];
+    }
+
+    return (objective_functions);
+  }
+
+  inline unsigned int
+  LineSearch::get_n_iterations() const
+  {
+    return (n_iterations);
+  }
 
 } // namespace LineSearch
-
-
 
 #endif /* INCLUDE_LINE_SEARCH_H_ */
--- source/line_search.cc
+++ source/line_search.cc
@@ -27,8 +27,8 @@ LineSearch::LineSearch(
 :
 n_iterations(0),
 n_max_iterations(parameters.n_max_iterations),
-alpha(parameters.armijo_condition_constant),
-beta(0.9)
+alpha(parameters.alpha),
+beta(parameters.beta)
 {
   table_handler.declare_column("S");
   table_handler.declare_column("N-Itr");
@@ -45,11 +45,61 @@ void LineSearch::reinit(
   const unsigned int  iteration_id)
 {
   this->n_iterations                  = 0;
+
   this->initial_scalar_function_value = initial_scalar_function_value;
+
   this->descent_direction             = - 2. *
                                           initial_scalar_function_value;
+
   this->step_id                       = step_id;
+
   this->iteration_id                  = iteration_id;
+}
+
+
+
+void LineSearch::reinit(
+  const std::vector<double> initial_scalar_function_values,
+  const unsigned int        step_id,
+  const unsigned int        iteration_id)
+{
+  this->n_iterations = 0;
+
+  this->initial_scalar_function_values =
+    initial_scalar_function_values;
+
+  this->descent_directions =
+    std::vector<double>(initial_scalar_function_values.size(), 0.0);
+
+  for (unsigned int i = 0;
+       i < initial_scalar_function_values.size();
+       ++i)
+  {
+    this->descent_directions[i] =
+      -2.0 * initial_scalar_function_values[i];
+  }
+
+  this->lambdas =
+    std::vector<double>(initial_scalar_function_values.size(), 0.0);
+
+  this->old_lambdas =
+    std::vector<double>(initial_scalar_function_values.size(), 0.0);
+
+  this->old_old_lambdas =
+    std::vector<double>(initial_scalar_function_values.size(), 0.0);
+
+  this->old_scalar_function_values =
+    std::vector<double>(initial_scalar_function_values.size(), 0.0);
+
+  this->old_old_scalar_function_values =
+    std::vector<double>(initial_scalar_function_values.size(), 0.0);
+
+  this->flag_sufficient_descent_conditions =
+    std::vector<bool>(initial_scalar_function_values.size(), false);
+
+  this->step_id = step_id;
+
+  this->iteration_id = iteration_id;
 }
 
 
@@ -89,6 +139,64 @@ double LineSearch::get_lambda(const double trial_scalar_function_value,
 
 
 
+std::vector<double> LineSearch::get_lambdas(
+  const std::vector<double> trial_scalar_function_values,
+  const std::vector<double> trial_lambdas)
+{
+  for (unsigned int i = 0; i < trial_lambdas.size(); ++i)
+  {
+    AssertIsFinite(trial_scalar_function_values[i]);
+    AssertIsFinite(trial_lambdas[i]);
+
+    if (!flag_sufficient_descent_conditions[i])
+    {
+      old_old_scalar_function_values[i] = old_scalar_function_values[i];
+      old_scalar_function_values[i] = trial_scalar_function_values[i];
+
+      if (n_iterations == 0)
+      {
+        old_lambdas[i] = trial_lambdas[i];
+        lambdas[i] = quadratic_backtracking(
+                      trial_scalar_function_values[i], i);
+      }
+      else
+      {
+        old_old_lambdas[i]  = old_lambdas[i];
+        old_lambdas[i]      = trial_lambdas[i];
+        lambdas[i]          = cubic_backtracking(i);
+      }
+    }
+    else
+    {
+      if (n_iterations == 0)
+      {
+        old_lambdas[i] = trial_lambdas[i];
+      }
+      else
+      {
+        old_old_lambdas[i]  = old_lambdas[i];
+        old_lambdas[i]      = trial_lambdas[i];
+      }
+
+      lambdas[i] = trial_lambdas[i];
+    }
+
+    AssertIsFinite(lambdas[i]);
+  }
+
+
+  n_iterations++;
+
+  table_handler.add_value("S", step_id);
+  table_handler.add_value("N-Itr", iteration_id);
+  table_handler.add_value("L-Itr", n_iterations);
+  table_handler.add_value("Rlx-Prm", lambda);
+
+  return lambdas;
+}
+
+
+
 void LineSearch::write_to_file(const std::string filepath)
 {
   std::ofstream file(filepath);
@@ -108,6 +216,25 @@ double LineSearch::quadratic_backtracking(const double trial_scalar_function_val
                   (trial_scalar_function_value -
                    initial_scalar_function_value -
                    descent_direction);
+
+  if (lambda < 0.1)
+    return 0.1;
+  else if (lambda > 0.5)
+    return 0.5;
+  else
+    return lambda;
+}
+
+
+
+double LineSearch::quadratic_backtracking(
+  const double trial_scalar_function_value,
+  const unsigned int id)
+{
+  double lambda = - descent_directions[id] / 2.0 /
+                  (trial_scalar_function_value -
+                   initial_scalar_function_values[id] -
+                   descent_directions[id]);
 
   if (lambda < 0.1)
     return 0.1;
@@ -155,6 +282,48 @@ double LineSearch::cubic_backtracking()
     return 0.1*old_lambda;
   else if (lambda > 0.5*old_lambda)
     return 0.5*old_lambda;
+  else
+    return lambda;
+}
+
+
+
+double LineSearch::cubic_backtracking(const unsigned int id)
+{
+  const double factor = 1./(old_lambdas[id] - old_old_lambdas[id]);
+
+  dealii::FullMatrix<double> matrix;
+  matrix.reinit(2,2);
+  matrix[0][0] = 1./(old_lambdas[id] * old_lambdas[id]);
+  matrix[0][1] = - 1./(old_old_lambdas[id] * old_old_lambdas[id]);
+  matrix[1][0] = -old_old_lambdas[id]/(old_lambdas[id] * old_lambdas[id]);;
+  matrix[1][1] = old_lambdas[id]/(old_old_lambdas[id] * old_old_lambdas[id]);
+
+  dealii::Vector<double> vector;
+  vector.reinit(2);
+  vector[0] = old_scalar_function_values[id] -
+              initial_scalar_function_values[id] -
+              old_lambdas[id] * descent_directions[id];
+  vector[1] = old_old_scalar_function_values[id] -
+              initial_scalar_function_values[id] -
+              old_old_lambdas[id] * descent_directions[id];
+
+  dealii::Vector<double> coefficients;
+  coefficients.reinit(2);
+
+  matrix.vmult(coefficients, vector);
+
+  coefficients *= factor;
+
+  double lambda = (-coefficients[1] +
+                  std::sqrt(coefficients[1]*coefficients[1] -
+                            3.0 * coefficients[0] *descent_directions[id])) /
+                  (3. * coefficients[0]);
+
+  if (lambda < 0.1*old_lambdas[id])
+    return 0.1*old_lambdas[id];
+  else if (lambda > 0.5*old_lambdas[id])
+    return 0.5*old_lambdas[id];
   else
     return lambda;
 }
