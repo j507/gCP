77859e909f774d1dc0830b9f1df9f3a39a41b5fb
j507 <jose.a.lara.h@gmail.com>
2025-02-07 17:23:29 +0100

    (F) Compilation error

--- source/gradient_crystal_plasticity/solve.cc
+++ source/gradient_crystal_plasticity/solve.cc
@@ -1492,48 +1492,6 @@ double GradientCrystalPlasticitySolver<dim>::
 
 
 template <int dim>
-double GradientCrystalPlasticitySolver<dim>::
-line_search_algorithm(
-  dealii::Vector<double> residual_l2_norms,
-  const BlockIndex block_index)
-{
-  const unsigned int block_id = static_cast<unsigned int>(block_index);
-
-  double relaxation_parameter = 1.0,
-         objective_function_value =
-            LineSearch::get_objective_function_value(
-              residual_l2_norms[block_id]);
-
-  while (!line_search->suficient_descent_condition(
-      objective_function_value, relaxation_parameter))
-  {
-    relaxation_parameter =
-      line_search->get_lambda(objective_function_value,
-                                relaxation_parameter);
-
-    reset_trial_solution();
-
-    update_trial_solution(relaxation_parameter, block_id);
-
-    reset_and_update_quadrature_point_history();
-
-    reset_and_update_slip_resistances();
-
-    assemble_residual();
-
-    residual_l2_norms = fe_field->get_sub_l2_norms(residual);
-
-    objective_function_value =
-      LineSearch::get_objective_function_value(
-        residual_l2_norms[block_id]);
-  }
-
-  return relaxation_parameter;
-}
-
-
-
-template <int dim>
 void GradientCrystalPlasticitySolver<dim>::store_trial_solution(
   const bool flag_store_initial_trial_solution)
 {
